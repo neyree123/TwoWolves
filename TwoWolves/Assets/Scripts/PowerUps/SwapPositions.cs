da35c691132b62acfc7fd1ff2dcb04395283a448
Mason Hayes (RIT Student) <mbh9947@rit.edu>
2022-01-29 13:54:14 -0500

    Added the speed and swap position power ups

---
+++ TwoWolves/Assets/Scripts/PowerUps/SwapPositions.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapPositions : MonoBehaviour, IPowerUps
+{
+
+    //Variables
+    GameObject[] players;
+    Vector3[] playerPos = new Vector3[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Grab the players
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+    }
+
+    public void ActivatePower()
+    {
+        //Set the initial positions
+        playerPos[0] = players[0].transform.position;
+        playerPos[1] = players[1].transform.position;
+
+        //Swap positions
+        players[0].transform.position = playerPos[1];
+        players[1].transform.position = playerPos[0];
+
+        //Destroy pickup
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Make sure only the players can activate the powerup
+        if (collision.CompareTag("Player"))
+            ActivatePower();
+    }
+}
