4f418a819d57211bc1b767fe13e57936d2e7f721
Destiny Treptau <treptaudestiny@gmail.com>
2022-01-29 10:22:42 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ TwoWolves/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool move = false;
+    private Vector2 moveDir;
+    private Rigidbody2D rb;
+    public float speed = 5f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDir * speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveDir = value.Get<Vector2>();
+    }
+}
