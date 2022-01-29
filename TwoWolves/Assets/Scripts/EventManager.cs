a2b81f87fc62facc99151aa69dca39197e708d4a
Mason Hayes (RIT Student) <mbh9947@rit.edu>
2022-01-29 16:52:44 -0500

    Working on the method to subscribe to the event manager

--- TwoWolves/Assets/Scripts/EventManager.cs
+++ TwoWolves/Assets/Scripts/EventManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class EventManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static EventManager instance;
+
+    private void Awake()
     {
-        
+        if (!instance)
+            instance = this;
+        else
+            Destroy(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public event Action EndGame;
+
+    public void TriggerEndGame()
     {
-        
+        EndGame?.Invoke();
     }
 }
