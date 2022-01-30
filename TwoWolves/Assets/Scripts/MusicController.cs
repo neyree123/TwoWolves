56e27404b8ce6e284ef2fee9ae3abecb00b661a1
Candygonemad <candygonemad@gmail.com>
2022-01-30 12:59:56 -0500

    Music

---
+++ TwoWolves/Assets/Scripts/MusicController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if(musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
